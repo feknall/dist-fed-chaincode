cf8478d3d2a6fcd88cff9c11cce562cb5fc502a3
feknall <h.fazli.k@gmail.com>
2022-11-08 12:18:06 -0500

    Fix cccg problem

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./test-network/network.sh up createChannel -c mychannel -ca
-./test-network/network.sh deployCCAAS  -ccn dist-fed-chaincode -ccp .. -ccep "OR('Org1MSP.peer','Org2MSP.peer')" -cccg './collections_config.json'
+./test-network/network.sh deployCCAAS  -ccn dist-fed-chaincode -ccp .. -ccep "OR('Org1MSP.peer','Org2MSP.peer')" -cccg '../collections_config.json'
 
